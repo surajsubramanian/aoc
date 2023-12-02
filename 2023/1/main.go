a93764a26506770663b862f99887216446153dad
surajsubramanian <surajsubramanian2000@gmail.com>
2023-12-02 15:05:21 +0200

    [2023/1] Use strings.IndexAny and strings.LastIndexAny

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -1,47 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func lineDoer(t string) int {
-	start, end := 0, len(t)-1
 	digits := "0123456789"
-	for i := 0; i < len(t); i++ {
-		l := t[i]
-		if strings.Contains(digits, string(l)) {
-			start, _ = strconv.Atoi(string(l))
-			break
-		}
-	}
-	for i := len(t) - 1; i >= 0; i-- {
-		l := t[i]
-		if strings.Contains(digits, string(l)) {
-			end, _ = strconv.Atoi(string(l))
-			break
-		}
-	}
-	return (start*10 + end)
+	start := t[strings.IndexAny(t, digits)] - '0'
+	end := t[strings.LastIndexAny(t, digits)] - '0'
+	return int(start)*10 + int(end)
 }
 
 func main() {
-	file, err := os.Open("temp.txt")
-	if err != nil {
-		log.Fatal(err)
+	file, _ := os.ReadFile("temp.txt")
+	result := 0
+	for _, line := range strings.Split(string(file), "\n") {
+		if line != "" {
+			result += lineDoer(line)
+		}
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	ans := 0
-	for scanner.Scan() {
-		ans += lineDoer(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	log.Println(ans)
+	log.Println(result)
 }
