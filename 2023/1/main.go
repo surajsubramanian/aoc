726d7405725ece3e78dcf055ef674cd01c26eb28
surajsubramanian <surajsubramanian2000@gmail.com>
2023-12-02 15:44:11 +0200

    Replace every number like - 2 with two2two, to counter cases like eightwo

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -6,19 +6,41 @@ import (
 	"strings"
 )
 
-func lineDoer(t string) int {
+func LineCalibrationValue(t string) int {
 	digits := "0123456789"
 	start := t[strings.IndexAny(t, digits)] - '0'
 	end := t[strings.LastIndexAny(t, digits)] - '0'
 	return int(start)*10 + int(end)
 }
 
+func Replacer(line string) string {
+	dict := map[string]string{
+		"zero":  "zero0zero",
+		"one":   "one1one",
+		"two":   "two2two",
+		"three": "three3three",
+		"four":  "four4four",
+		"five":  "five5five",
+		"six":   "six6six",
+		"seven": "seven7seven",
+		"eight": "eight8eight",
+		"nine":  "nine9nine",
+	}
+
+	for k, v := range dict {
+		line = strings.ReplaceAll(line, k, v)
+	}
+	return line
+}
+
 func main() {
 	file, _ := os.ReadFile("temp.txt")
 	result := 0
+
 	for _, line := range strings.Split(string(file), "\n") {
+		line = Replacer(line)
 		if line != "" {
-			result += lineDoer(line)
+			result += LineCalibrationValue(line)
 		}
 	}
 	log.Println(result)
--- 2023/1/main_test.go
+++ 2023/1/main_test.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestDoer(t *testing.T) {
-	s := "two2seven7"
-	x := lineDoer(s)
-	if x != 27 {
-		t.Errorf("expected 27, got %d", x)
+func TestReplacer(t *testing.T) {
+	s := []struct {
+		input  string
+		starts string
+		ends   string
+	}{
+		{
+			"two2seven7",
+			"two2",
+			"seven7",
+		},
+		{
+			"eightwothree",
+			"eight8",
+			"3three",
+		},
+	}
+	for _, tt := range s {
+		if got := Replacer(tt.input); !strings.HasPrefix(got, tt.starts) || !strings.HasSuffix(got, tt.ends) {
+			t.Errorf("Replacer() = %v should start with %v and end with %v", got, tt.starts, tt.ends)
+		}
+	}
+}
+
+func TestLineCalibrationValue(t *testing.T) {
+	s := []struct {
+		input  string
+		output int
+	}{
+		{
+			"two2seven7",
+			27,
+		},
+		{
+			"eightwothree",
+			83,
+		},
+	}
+	for _, tt := range s {
+		got := LineCalibrationValue(Replacer(tt.input))
+		if got != tt.output {
+			t.Errorf("LineDoer() = %v, want %v", got, tt.output)
+		}
 	}
 }
